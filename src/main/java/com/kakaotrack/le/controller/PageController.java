040e4c4073cc284191fcc4e5417d6827c2fdbad3
Wef2 <qhrud5725@hanmail.net>
2016-01-18 18:35:33 +0900

    Add thymeleaf

--- src/main/java/com/kakaotrack/le/controller/PageController.java
+++ src/main/java/com/kakaotrack/le/controller/PageController.java
@@ -1,11 +1,16 @@
 package com.kakaotrack.le.controller;
 
 import org.springframework.stereotype.Controller;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Created by Kim on 2016-01-18.
  */
 @Controller
 public class PageController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
 
 }
